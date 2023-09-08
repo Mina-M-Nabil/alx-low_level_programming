3f4b9b8ba748f8a61db595d62e19b5bbf77aa9cd
Mina Mamdouh Nabil <mina.mamdouh.nabil@gmail.com>
2023-09-08 15:55:23 -0700

    6th C program for this project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints number from 0 to 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d\n", i);
+	return (0);
+}
