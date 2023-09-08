1335187c3c49fd9cd315741447edf77fa4a3e08e
Mina Mamdouh Nabil <mina.mamdouh.nabil@gmail.com>
2023-09-08 16:51:28 -0700

    9th C program for this project

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints base 16 numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+{
+	int i = '0';
+	char ch = 'a';
+
+	for (i = '0'; i <= 9; i++)
+		putchar(i);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
