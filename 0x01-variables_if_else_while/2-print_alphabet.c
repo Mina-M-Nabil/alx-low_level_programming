887f2283ea5f958ac83fa7cb1c38e5bea17a5bfc
Mina Mamdouh Nabil <mina.mamdouh.nabil@gmail.com>
2023-09-08 14:39:53 -0700

    3rd C program for this project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphaptic characters
+ *	using putchar function
+ *
+ * Return: Always 1 (Success)
+*/
+
+
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar(ch"\n");
+	return (0);
+}
